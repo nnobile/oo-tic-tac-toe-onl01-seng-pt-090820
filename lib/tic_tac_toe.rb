db86ead8f4501201e70c77d212881efafec120b5
Nick Nobile <nickcnobile@gmail.com>
2020-10-01 03:03:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,15 +50,16 @@ class TicTacToe
   end
 
   def turn
-     puts "Please enter 1-9:"
-     user_input = gets.strip
-     index = input_to_index(user_input)
-     if valid_move?(index)
-       move(index, current_player)
-     else
-       turn
+       puts "Please enter 1-9:"
+       user_input = gets.strip
+       index = input_to_index(user_input)
+       if valid_move?(index)
+         move(index, current_player)
+       else
+         turn
+       end
+       display_board
      end
-   end
 
   def turn_count
   #returns the number of turns that have been played based on the board variable
